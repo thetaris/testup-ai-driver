0c651c358821e9afddac1420e89b1085078267b9
Amer Qarabsa <qarabsa@thetaris.com>
2024-02-08 22:37:07 +0100

    provide example to return id

--- src/action_processor.py
+++ src/action_processor.py
@@ -57,6 +57,21 @@ class DomAnalyzer:
     session_histories = {}
     system_prompt = """
     You are a testautomation system. Your job is to analyze the already executed actions and determine the next actions needed to complete the provided task.
+    
+    The actions that you can take are:
+        1. click (if you need to click something on the screen)
+        2. enter_text (if you believe you need to write something)
+        3. scroll (if you are instructed to scroll or scrolling is needed to complete action)
+        4. finish (at the end to know that we are done or if all actions have been executed)
+        5. error ( the given task cannot be accomplished)
+
+     Each entry is an object of 5 fields, the fields are the following:
+        1. action: can be one of: click, enter_text, wait, error, or finish.
+        2. css_selector: (only needed for click or enter-text), this is the css id of the html element('li', 'button', 'input', 'textarea', 'a'), example #id.
+        3. text: this is optional and contains the text you want to input in case of an enter-text action. 
+        4. explanation: this is why you chose this action.
+        5. description: detailed description of the action
+        The output format should be {"steps":[{ "action":..,"css_selector":...., "text":..., "explanation":..., "description":...}]}
     """
 
     def __init__(self):
@@ -64,65 +79,6 @@ class DomAnalyzer:
         if self.gpt_model not in api_map:
             raise ValueError(f"Model '{self.gpt_model}' is not supported")
 
-    def get_actions_by_device(self, deviceId, user_prompt, html_doc, actions_executed):
-
-        logging.info(f"System input: {self.gpt_prompt}")
-        markdown_content = convert_to_md(html_doc)
-
-        # removing unneeded spaces
-        logging.info(f"Markdown: {markdown_content}")
-        user_content = f"Here is the Markdown: {markdown_content}."
-
-        system_prompt = self.gpt_prompt
-        if actions_executed:
-            system_prompt += f"\nAnd these are the already executed actions that we no longer need to execute. They should not be in the output at all: {actions_executed}"
-
-        system_prompt += f"\nAnd this is your task:\nImagine you already executed the given list of \"previous actions\", what actions remain to complete the following task:\n{user_prompt}"
-        api_info = api_map_json[self.gpt_model]
-        payload = api_info['payload'](self.gpt_model, system_prompt, user_content)
-        logging.info(f"sending request {payload}")
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.gpt_api_key}"
-        }
-
-        # Send POST request to OpenAI API
-        response = requests.post(api_info['endpoint'], headers=headers, json=payload)
-
-        response_data = response.json()
-
-        logging.info(f"Response from openai {response_data}")
-
-        response_object_type = response_data.get('object', '')
-
-        if "choices" in response_data and len(response_data["choices"]) > 0:
-            if response_object_type == 'chat.completion':
-                # Handling response for 'chat.completion'
-                assistant_message_json_str = response_data["choices"][0].get("message", {}).get("content", "")
-            elif response_object_type == 'text_completion':
-                # Handling response for 'text_completion'
-                assistant_message_json_str = response_data["choices"][0].get("text", "")
-            else:
-                raise Exception("Unknown response object type.")
-
-            total_tokens = response_data["usage"].get("total_tokens", 0)
-
-            try:
-                # Parse the extracted content as JSON
-                logging.info(f"assistant_message_json_str = {assistant_message_json_str}")
-                assistant_message_json_str = assistant_message_json_str.replace("```json", "").replace("```", "").strip()
-
-                assistant_message = json.loads(assistant_message_json_str)
-            except json.JSONDecodeError:
-                raise Exception("Error decoding the extracted content as JSON.")
-
-            logging.info(f"Tokens: {total_tokens}")
-            # Store in new JSON object
-
-            logging.info(f"Returning: {assistant_message}")
-            return assistant_message
-        else:
-            raise Exception(f"No content found in response or invalid response format:{response_data}")
 
     def get_actions(self, session_id, user_prompt, html_doc, actions_executed):
 
@@ -131,7 +87,7 @@ class DomAnalyzer:
         # removing unneeded spaces
         logging.info(f"Markdown: {markdown_content}")
 
-        user_content = self.system_rules
+        user_content = ""
         assistant_prompt = []
         assistant_prompt.extend(action for action in actions_executed)
         logging.info(f"Assistant prompt: {assistant_prompt}")
@@ -142,11 +98,9 @@ class DomAnalyzer:
 
         user_content += "\n\nAnd this is your task: "
         user_content += "\nImagine you already executed the given list of \"previous actions\", what actions remain to complete the following task:"
-        user_content += f"\n- {user_prompt}"
+        user_content += f"\n- {user_prompt}\n"
 
-        user_content += f"Here is the Markdown: {markdown_content}."
-        user_content += "\nYou are a browser automation assistant. Your job is to analyze the already executed actions and determine the next actions needed to complete the provided task."
-
+        user_content += f"\nHere is the Markdown: {markdown_content}."
 
         api_info = api_map_json[self.gpt_model]
         payload = api_info['payload'](self.gpt_model, self.system_prompt, user_content, assistant_prompt)
