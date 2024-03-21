9002774ebb4756a55c1a792df159eaab60fe7d93
Raphael Altemeyer <altemeyer@thetaris.com>
2024-03-21 10:57:20 +0100

    removed file that was not needed anymore. Improved example_testup_mywebshop.py

--- tests/selenium/example_testup_mywebshop.py
+++ tests/selenium/example_testup_mywebshop.py
@@ -1,4 +1,4 @@
-from datetime import time
+import time
 from selenium.webdriver.common.by import By
 from selenium_utils import SeleniumUtils
 from selenium import webdriver
@@ -28,18 +28,28 @@ try:
 
     mySelenium.execute_prompt("""put any product in shopping cart""")
     time.sleep(1)
+    mySelenium.execute_prompt("""go to the shopping cart""")
+    try:
+        # Find the element
+        input_element = mySelenium.driver.find_element(By.CLASS_NAME, 'input-text.qty.text')
+        # Get the value attribute
+        quantity_value = int(input_element.get_attribute('value'))
+        if not quantity_value == 1:
+            raise Exception("product not added to cart")
+        else:
+            print("Product has been added to cart")
+    except:
+        print("did not navigate to shopping cart")
+    mySelenium.execute_prompt("""go to the checkout page, the button to do so is at the bottom of the shopping cart page""")
+    # Find the <h1> tag directly using a CSS selector
+    title_element = driver.find_element(By.CSS_SELECTOR, 'header.entry-header h1.entry-title')
+    title_text = title_element.text
 
-    # Find the element
-    input_element = mySelenium.driver.find_element(By.CLASS_NAME, 'input-text.qty.text')
-    # Get the value attribute
-    quantity_value = input_element.get_attribute('value')
-    if not quantity_value == 1:
-        raise Exception("product not added to cart")
-
-    mySelenium.execute_prompt("""go to checkout""")
+    if title_text != "Checkout":
+        print("did not navigate to checkout, test failed")
+    else:
+        print("Successfully navigated to checkout")
     time.sleep(1)
-
-
 
 
     ###############################################
